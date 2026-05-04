0d544f200f8d183711fdbf924d6a3d1950ebedee
ZaynTamam <tamam.zain78@gmail.com>
2026-05-04 11:25:21 +0700

    Menambahkan definisi constructor dengan parameter pada kelas mahasiswa di file constructorberparameter.cpp

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -9,3 +9,10 @@ public:
 mahasiswa::mahasiswa() {
     cout << "Constructor Terpanggil" << endl; // Definisi Constructor
 }
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
