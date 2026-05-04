c1a841edb475ec75830e172a5cb7eea052ab241e
ZaynTamam <tamam.zain78@gmail.com>
2026-05-04 11:25:38 +0700

    Menambahkan fungsi main untuk menguji kelas mahasiswa dan konstruktor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -16,3 +16,10 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "NIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
 }
+
+
+int main(){
+    mahasiswa mhs; // Pembuatan Object
+    mahasiswa mhs2(12345, "Pascal"); // memanggil constructor dengan parameter
+    return 0;
+}
