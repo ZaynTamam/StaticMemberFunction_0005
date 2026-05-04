716a2a89aed254c30d2f47588363944830b1036e
ZaynTamam <tamam.zain78@gmail.com>
2026-05-04 11:28:20 +0700

    Menambahkan definisi kelas buku beserta fungsi setget di file this.cpp

--- this.cpp
+++ this.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+class buku {
+public:
+    string judul;
+
+    string setget(string judul) {
+        this->judul = judul; // nilai parameter 'judul' untuk member variabel
+        return this->judul;  // return variabel judul
+    }
+};
